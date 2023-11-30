3ec0390965ee727a4c823b0757a0e68527ff4d8b
Gwasagir <keurulainen.niko@gmail.com>
2023-11-30 13:44:18 +0200

    expanded file_service test coverage

--- src/tests/file_service_test.py
+++ src/tests/file_service_test.py
@@ -19,3 +19,17 @@ class TestFileService(unittest.TestCase):
         self.file_service.add_bibtex_file(self.file_name)
         pl.Path(f"./{self.file_name}")
         self.cleanup()
+
+    def test_bibfile_error_if_exists(self):
+        self.file_service.add_bibtex_file(self.file_name)
+        fail = self.file_service.add_bibtex_file(self.file_name)
+        self.assertAlmostEqual(fail, f"{self.file_name} is already existent.\n")
+
+    def test_file_deteled_succesfully(self):
+        self.file_service.add_bibtex_file(self.file_name)
+        success = self.file_service.remove_bibtex_file(self.file_name)
+        self.assertAlmostEqual(success, f"{self.file_name} removed succesfully.\n")
+
+    def test_non_existent_file_delete(self):
+        fail = self.file_service.remove_bibtex_file(self.file_name)
+        self.assertAlmostEqual(fail, "File not found\n")
